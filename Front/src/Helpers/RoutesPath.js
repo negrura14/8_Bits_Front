407c8602c4a99bd5b6643a2961f0d2b2531aebbf
negrura14 <talleresmatador114@gmail.com>
2023-08-30 21:12:54 -0300

    Rutas

---
+++ Front/src/Helpers/RoutesPath.js
@@ -0,0 +1,10 @@
+export const ROUTES = {
+    LANDING: '/',
+    HOME: '/Home/',
+    DETAIL: '/Home/:id',
+    NAME: '/Home/:name',
+    ABOUT: '/About',
+    STORE:'/store',
+    ERROR: '*',
+   
+}
