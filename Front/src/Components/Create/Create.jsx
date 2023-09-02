0ec07734e6079105a8da4e2c9c3587d705eb5b62
Camuzzi <camuzziagustin@gmail.com>
2023-09-02 17:49:12 -0300

    Create avanzado y archivo validate agregado

--- Front/src/Components/Create/Create.jsx
+++ Front/src/Components/Create/Create.jsx
@@ -2,11 +2,18 @@ import "./Create.css";
 
 import { useState,useEffect } from "react";
 import {useSelector,useDispatch} from "react-redux";
+import validate from "./validate";
 
 export default function Create() {
 
     //const dispatch = useDispatch();
     // acá tendríamos que traer a todos los videojuegos para luego validar si estamos cargando uno con el mismo nombre
+
+    //arrays de prueba
+    const names = ["GTA","Mario","Need for Speed","Call of Duty","Battlefield"];
+
+    const initialPlatforms = ["PC", "PS3", "PS2", "XBOX"];
+    const initialGenres = ["Action", "Adventure", "RPG", "Puzzle", "First-Person Shooter"];
 
 
     // Estados locales para manejar los input y los errores:
@@ -20,6 +27,7 @@ export default function Create() {
         genre: [],
         price: "",
         stock: "",
+        review: ""
     });
 
     const [errors,setErrors] = useState({
@@ -31,23 +39,92 @@ export default function Create() {
         genre: [],
         price: "",
         stock: "",
+        review: ""
     });
 
-    return(
-        <div>
+    //local states to manage the platforms and genres
+
+    const [selectedPlatform, setSelectedPlatform] = useState("");
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const [selectedPlatforms, setSelectedPlatforms] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+    
+  function handleAddPlatform() {
+    if (selectedPlatform && !selectedPlatforms.includes(selectedPlatform)) {
+      setSelectedPlatforms([...selectedPlatforms, selectedPlatform]);
+      setInput({
+        ...input,
+        supportedPlatforms: [...input.supportedPlatforms, selectedPlatform], // Agregar a la propiedad supportedPlatforms
+      });
+      setSelectedPlatform("");
+    }
+  }
+
+  function handleRemovePlatform(platform) {
+    const updatedPlatforms = selectedPlatforms.filter((p) => p !== platform);
+    setSelectedPlatforms(updatedPlatforms);
+    setInput({
+      ...input,
+      supportedPlatforms: updatedPlatforms, // Actualizar la propiedad supportedPlatforms
+    });
+  }
+
+  function handleAddGenre() {
+    if (selectedGenre && !selectedGenres.includes(selectedGenre)) {
+      setSelectedGenres([...selectedGenres, selectedGenre]);
+      setInput({
+        ...input,
+        genre: [...input.genre, selectedGenre], // Agregar a la propiedad genre
+      });
+      setSelectedGenre("");
+    }
+  }
+
+  function handleRemoveGenre(genre) {
+    const updatedGenres = selectedGenres.filter((g) => g !== genre);
+    setSelectedGenres(updatedGenres);
+    setInput({
+      ...input,
+      genre: updatedGenres, // Actualizar la propiedad genre
+    });
+  }
+
+
+        //end platforms and genres functions
+        
+        function handleChange(e) {
+            e.preventDefault();
+    
+            setInput({
+                ...input,
+                [e.target.name]: e.target.value
+            })
+    
+            setErrors(
+                validate({
+                    ...input,
+                    [e.target.name]: e.target.value
+                },names)
+            )
+        }
+        
+        
+        return(
+            <div>
             <h1>Create Game</h1>
 
             <form>
 
                 <label>Name</label>
                 <input 
-                    placeholder="Enter Title"
+                    placeholder="Enter Name"
                     type="text"
                     name="name"
                     value={input.name}
-                    // onChange={(event) => handleChange(event)}
+                    onChange={(event) => handleChange(event)}
                     // className={errors.name ? 'error' : ''}                    
                 />
+                {errors.name && <p className="error-message">{errors.name}</p>}
 
                 <label>Description</label>
                 <input 
@@ -55,9 +132,10 @@ export default function Create() {
                     type="text"
                     name="description"
                     value={input.description}
-                    // onChange={(event) => handleChange(event)}
-                    // className={errors.name ? 'error' : ''}                    
+                    onChange={(event) => handleChange(event)}
+                    // className={errors.description ? 'error' : ''}                    
                 />
+                {errors.description && <p className="error-message">{errors.description}</p>}
 
                 <label>Price</label>
                 <input 
@@ -65,46 +143,82 @@ export default function Create() {
                     type="number"
                     name="price"
                     value={input.price}
-                    // onChange={(event) => handleChange(event)}
-                    // className={errors.name ? 'error' : ''}                    
+                    onChange={(event) => handleChange(event)}
+                    // className={errors.price ? 'error' : ''}                    
                 />
 
-                <label>Weight</label>
-                <input 
-                    placeholder="Enter Weight"
-                    type="number"
-                    name="weight"
-                    value={input.weight}
-                    // onChange={(event) => handleChange(event)}
-                    // className={errors.name ? 'error' : ''}                    
-                />
-
-                <label>Date</label>
+                <label>Release Date</label>
                 <input 
                     placeholder="Enter Date"
-                    type="text"
+                    type="date"
                     name="releaseDate"
                     value={input.releaseDate}
-                    // onChange={(event) => handleChange(event)}
-                    // className={errors.name ? 'error' : ''}                    
+                    onChange={(event) => handleChange(event)}
+                    // className={errors.releaseDate ? 'error' : ''}                    
                 />
 
-                <label>Platforms</label>
-                <input 
-                    placeholder="Enter Supported Platforms"
-                    type="text"
-                    name="supportedPlatforms"
-                    value={input.supportedPlatforms}
-                    // onChange={(event) => handleChange(event)}
-                    // className={errors.name ? 'error' : ''}                    
-                />
+<div>
+          <label>Supported Platforms</label>
+          <select
+            value={selectedPlatform}
+            onChange={(event) => setSelectedPlatform(event.target.value)}
+          >
+            <option value="">Select Platform</option>
+            {initialPlatforms.map((platform) => (
+              <option key={platform} value={platform}>
+                {platform}
+              </option>
+            ))}
+          </select>
+          <button type="button" onClick={handleAddPlatform}>
+            Add
+          </button>
+          <div>
+            {selectedPlatforms.map((platform) => (
+              <div key={platform}>
+                <span>{platform}</span>
+                <button type="button" onClick={() => handleRemovePlatform(platform)}>
+                  Remove
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
 
-                <label>Genre</label>
+        <div>
+          <label>Genre</label>
+          <select
+            value={selectedGenre}
+            onChange={(event) => setSelectedGenre(event.target.value)}
+          >
+            <option value="">Select Genre</option>
+            {initialGenres.map((genre) => (
+              <option key={genre} value={genre}>
+                {genre}
+              </option>
+            ))}
+          </select>
+          <button type="button" onClick={handleAddGenre}>
+            Add
+          </button>
+          <div>
+            {selectedGenres.map((genre) => (
+              <div key={genre}>
+                <span>{genre}</span>
+                <button type="button" onClick={() => handleRemoveGenre(genre)}>
+                  Remove
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+
+                <label>Review</label>
                 <input 
-                    placeholder="Enter Genre"
+                    placeholder="Enter Review"
                     type="text"
-                    name="genre"
-                    value={input.genre}
+                    name="review"
+                    value={input.review}
                     // onChange={(event) => handleChange(event)}
                     // className={errors.name ? 'error' : ''}                    
                 />
