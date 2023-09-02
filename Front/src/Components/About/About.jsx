6d0e2612f4b11420a8582675417911f3a663a598
V C <107280002+VictorC1707@users.noreply.github.com>
2023-09-02 19:29:41 -0400

    estilos About y solucion de error en landing

--- Front/src/Components/About/About.jsx
+++ Front/src/Components/About/About.jsx
@@ -1,56 +1,80 @@
- import "./About.css";
- 
- export default function About() {
-    // pretendo generar un array con todos nuestros datos y luego con un map renderizar todos de una sóla vez
-    const data = [];
+import "./About.css";
 
-    return (
-        <div> 
+export default function About() {
+  // pretendo generar un array con todos nuestros datos y luego con un map renderizar todos de una sóla vez
+  const data = [];
 
-            <h1>About us</h1>
-
-            <div>
-
-                <img 
-                    src={"AVATAR"}
-                    alt="characterImg"
-                />
-                <h2>Agustin Camuzzi</h2>
-                <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam itaque iure veniam assumenda debitis aut quasi quisquam minima ipsam. Soluta voluptates reiciendis molestias sed ratione vero cupiditate veritatis, pariatur dolor!</h3>
-
-                <a
-                    href="INSTAGRAM LINK"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                />
-                <img
-                    src={"ICONO"}
-                    alt="Instagram"
-                />
-
-                <a
-                    href="GITHUB LINK"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                />
-                <img
-                    src={"ICONO"}
-                    alt="GitHub"
-                />
-
-                <a
-                    href="LINKED LINK"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                />
-                <img
-                    src={"ICONO"}
-                    alt="LinkedIn"
-                />
-
+  return (
+    <div>
+      <section>
+        <section class="team-page-section">
+          <div class="container">
+            <div class="sec-title centered">
+              <div class="title">Our Team</div>
+              <div class="separator">
+                <span></span>
+              </div>
+              <h2>About Us</h2>
             </div>
 
-        </div>
-    );
+            <div class="row clearfix">
+              <div class="team-block col-lg-4 col-md-6 col-sm-12">
+                <div
+                  class="inner-box wow fadeInLeft"
+                  data-wow-delay="0ms"
+                  data-wow-duration="1500ms"
+                >
+                  <ul class="social-icons">
+                    <li>
+                      <a href="#">
+                        <i class="fab fa-github"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#">
+                        <i class="fab fa-twitter"></i>
+                      </a>
+                    </li>
 
- }
+                    <li>
+                      <a href="#">
+                        <i class="fab fa-linkedin-in"></i>
+                      </a>
+                    </li>
+                  </ul>
+                  <div className="card2RM ">
+                    <div className="cardRM ">
+                      <div className="wrapper">
+                        <img
+                          src="https://i.pinimg.com/originals/53/29/53/5329530842a095a6ad61f3f5dd668ace.jpg"
+                          className="cover_image"
+                        />
+                      </div>
+                      <img
+                        src="https://art.pixilart.com/29c01efd2a4882e.png"
+                        className="character"
+                      />
+                    </div>
+                  </div>
+                  <div class="lower-content">
+                    <h3>
+                      <a href="#">Lorem Ipsum </a>
+                    </h3>
+                    <div class="designation">Simply dummy text </div>
+                  </div>
+                </div>
+              </div>
+              
+              
+            </div>
+          </div>
+        </section>
+      </section>
+    </div>
+  );
+}
