f2163da8443cc9ee92e6400298777e309b14bbd0
negrura14 <121406663+negrura14@users.noreply.github.com>
2023-09-04 18:11:04 -0300

    Update gameSlice.jsx

--- Front/src/Redux/gameSlice.jsx
+++ Front/src/Redux/gameSlice.jsx
@@ -18,14 +18,15 @@ export const gameSlice = createSlice({
             state.detail = []; // Establece detail en un objeto vacío al llamar a clearDetail
         },
         
-        
-        
         getGameByName: (state,action) => {
             state.detail = action.payload
+        },
+        filterGames: (state, action) => {
+            state.filter = action.payload
         }
     }
 })
 
-export const {getAllGame, getGameById, getGameByName, clearDetail} = gameSlice.actions
+export const {getAllGame, getGameById, getGameByName, clearDetail, filterGames } = gameSlice.actions
 
 export default gameSlice.reducer
