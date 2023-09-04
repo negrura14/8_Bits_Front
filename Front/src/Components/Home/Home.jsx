3b7791890680b474b88dc4a1cf1bb20c649ef6e4
Gerardo Campero <geragrungeeb@gmail.com>
2023-09-04 18:55:24 -0300

    Se cambia el useEffect del componente CARD a HOME

--- Front/src/Components/Home/Home.jsx
+++ Front/src/Components/Home/Home.jsx
@@ -1,14 +1,20 @@
 import './Home.css';
 import DateTimeDisplay from '../Time/Time'
 import Card from '../Card/Card';
+import { useDispatch } from 'react-redux';
+import { getGame } from '../../Redux/gameActions';
+import React, { useEffect } from 'react';
 
 
 
 function Home() {
+    const dispatch = useDispatch()
+    
     useEffect(()=>{
         dispatch(getGame())
       },[]) 
-    return(
+    
+      return(
         <div className='home_container'>
             <div>
                 <DateTimeDisplay />
