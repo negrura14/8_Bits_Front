2653e40060a4972456ee59e6808b21856041b2ed
negrura14 <talleresmatador114@gmail.com>
2023-09-04 17:52:39 -0300

    detail terminado

--- Front/src/Redux/gameSlice.jsx
+++ Front/src/Redux/gameSlice.jsx
@@ -13,12 +13,16 @@ export const gameSlice = createSlice({
         getGameById: (state,action) => {
             state.detail = action.payload
         },
+        clearDetail: (state) => {
+            state.detail = []; // Establece detail en un objeto vacío al llamar a clearDetail
+        },
+        
         getGameByName: (state,action) => {
             state.detail = action.payload
         }
     }
 })
 
-export const {getAllGame, getGameById, getGameByName} = gameSlice.actions
+export const {getAllGame, getGameById, getGameByName, clearDetail} = gameSlice.actions
 
 export default gameSlice.reducer
