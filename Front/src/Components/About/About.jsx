8c5b3ba9efac28230cc98edafd5242bf6ae9e5ac
Camuzzi <camuzziagustin@gmail.com>
2023-09-04 03:15:12 -0300

    Modificaciones en about y estilo basico en create

--- Front/src/Components/About/About.jsx
+++ Front/src/Components/About/About.jsx
@@ -1,49 +1,50 @@
 import "./About.css";
+import data from "./data";
 
 export default function About() {
-  // pretendo generar un array con todos nuestros datos y luego con un map renderizar todos de una sóla vez
-  const data = [];
 
   return (
     <div>
       <section>
-        <section class="team-page-section">
-          <div class="container">
-            <div class="sec-title centered">
-              <div class="title">Our Team</div>
-              <div class="separator">
+        <section className="team-page-section">
+          <div className="container">
+            <div className="sec-title centered">
+              <div className="title">Our Team</div>
+              <div className="separator">
                 <span></span>
               </div>
               <h2>About Us</h2>
             </div>
 
-            <div class="row clearfix">
-              <div class="team-block col-lg-4 col-md-6 col-sm-12">
+            {data.map((element) => (
+
+            <div className="row clearfix" key={element.name}>
+              <div className="team-block col-lg-4 col-md-6 col-sm-12">
                 <div
-                  class="inner-box wow fadeInLeft"
+                  className="inner-box wow fadeInLeft"
                   data-wow-delay="0ms"
                   data-wow-duration="1500ms"
                 >
-                  <ul class="social-icons">
+                  <ul className="social-icons">
                     <li>
-                      <a href="#">
-                        <i class="fab fa-github"></i>
+                      <a href={element.github}>
+                        <i className="fab fa-github"></i>
                       </a>
                     </li>
                     <li>
-                      <a href="#">
-                        <i class="fab fa-instagram"></i>
+                      <a href={element.instragram}>
+                        <i className="fab fa-instagram"></i>
                       </a>
                     </li>
                     <li>
-                      <a href="#">
-                        <i class="fab fa-twitter"></i>
+                      <a href={element.twitter}>
+                        <i className="fab fa-twitter"></i>
                       </a>
                     </li>
 
                     <li>
-                      <a href="#">
-                        <i class="fab fa-linkedin-in"></i>
+                      <a href={element.linkedIn}>
+                        <i className="fab fa-linkedin-in"></i>
                       </a>
                     </li>
                   </ul>
@@ -51,27 +52,31 @@ export default function About() {
                     <div className="cardRM ">
                       <div className="wrapper">
                         <img
-                          src="https://i.pinimg.com/originals/53/29/53/5329530842a095a6ad61f3f5dd668ace.jpg"
+                          src={element.avPhoto}
                           className="cover_image"
                         />
                       </div>
                       <img
-                        src="https://art.pixilart.com/29c01efd2a4882e.png"
+                        src={element.rePhoto}
                         className="character"
                       />
                     </div>
                   </div>
-                  <div class="lower-content">
+                  <div className="lower-content">
                     <h3>
-                      <a href="#">Lorem Ipsum </a>
+                      <a href="#">{element.name}</a>
                     </h3>
-                    <div class="designation">Simply dummy text </div>
+                    <div className="designation">{element.about}</div>
                   </div>
                 </div>
               </div>
-              
-              
             </div>
+            ))}
+
+
+
+
+
           </div>
         </section>
       </section>
