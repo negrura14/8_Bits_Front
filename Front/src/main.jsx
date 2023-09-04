ecfc534132939b367df1122292a064bb0e971df5
alejoT <yo.alejo.at@gmail.com>
2023-09-03 22:25:03 -0300

    Solución del problema que había con Nav

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
@@ -8,7 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
