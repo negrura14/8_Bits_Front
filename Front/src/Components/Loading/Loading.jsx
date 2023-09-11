1a8fb6cfa62f82ed9a6185cc2cbade9de1b661d7
V C <107280002+VictorC1707@users.noreply.github.com>
2023-09-11 16:52:29 -0400

    Loading, actualizcion Nav

--- Front/src/Components/Loading/Loading.jsx
+++ Front/src/Components/Loading/Loading.jsx
@@ -25,17 +25,23 @@ import Button from 'react-bootstrap/Button';
 
 export default function LoadingPage() {
     return (
-      <div className="loading-container">
-        <Button variant="primary" disabled>
-          <Spinner
-            as="span"
-            animation="grow"
-            size="lg"
-            role="status"
-            aria-hidden="true"
-          />
-          Loading...
-        </Button>
+      <div className="m-5 loading-container">
+        <div className="center row">
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+        </div>
+        <div className="center2 row text-center m-5">
+          <div className="loader">
+            <div className="scanner">
+              <span>Loading...</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
