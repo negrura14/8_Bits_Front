86d505ad91bfe4b39bc0e9d0d5baa2890676816e
V C <107280002+VictorC1707@users.noreply.github.com>
2023-09-19 17:01:15 -0400

    Vista de filter y loading

--- Front/src/Components/Loading/Loading.jsx
+++ Front/src/Components/Loading/Loading.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import './Loading.css';
-import Spinner from 'react-bootstrap/Spinner';
-import Button from 'react-bootstrap/Button';
+import logo from "../../Img/Logo.png"
 
 
 
@@ -25,23 +24,12 @@ import Button from 'react-bootstrap/Button';
 
 export default function LoadingPage() {
     return (
-      <div className="m-5 loading-container">
-        <div className="center row">
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-          <div className="wave"></div>
-        </div>
-        <div className="center2 row text-center m-5">
-          <div className="loader">
-            <div className="scanner">
-              <span>Loading...</span>
-            </div>
-          </div>
-        </div>
+      <>
+
+      <img className="logoL" src={logo}></img>
+      <div class="loading">
+      Loading&#8230;
       </div>
+      </>
     );
   }
