a8666a55b255397def797110facf9a0bb41056bf
TwilightMaster23 <briansilva1705@gmail.com>
2023-09-19 00:09:32 -0300

    Se importa el validateUser que faltaba

--- Front/src/Views/LandingPage/LandingPage.jsx
+++ Front/src/Views/LandingPage/LandingPage.jsx
@@ -14,6 +14,7 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
+import validateUser from "../../Components/Create User/validateUser";
 
 
 
