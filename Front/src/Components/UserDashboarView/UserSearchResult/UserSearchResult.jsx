0df49533e99f1276670133d99d55ffdc9a1cf29e
Gerardo Campero <geragrungeeb@gmail.com>
2023-09-22 19:28:06 -0300

    Se sube user management para el dasboard Admin

--- Front/src/Components/UserDashboarView/UserSearchResult/UserSearchResult.jsx
+++ Front/src/Components/UserDashboarView/UserSearchResult/UserSearchResult.jsx
@@ -1,9 +1,30 @@
+import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
+export default function UserSearchResult(props) {
+    const { disable, email, image, name, lastname, nickname} = props;
+    const [switcha, setSwitcha] = useState(false);
+    
+    const changeHandler = (event) => {
+        setSwitcha(!switcha)
+    }
 
-export default function UserSearchResult() {
     return(
-        <div>
-            Acá va el resultado de la búsqueda!
+        <div >
+            <img src={image} alt="Avatar" />
+            <span>{nickname}</span>
+            <span>{name}</span>
+            <span>{lastname}</span>
+            <span>{email}</span>
+            <Form>
+                <Form.Check // prettier-ignore
+                    type="switch"
+                    id="custom-switch"
+                    label="Disable User"
+                    checked={switcha}
+                    onChange={changeHandler}
+                />
+            </Form>
         </div>
     ) 
 }
