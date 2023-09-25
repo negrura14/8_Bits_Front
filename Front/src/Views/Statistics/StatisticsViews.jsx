e62e9bfa46a9b6852859b166ac6c6d7c17a1b0ea
TwilightMaster23 <briansilva1705@gmail.com>
2023-09-24 23:25:18 -0300

    Se crea una vista de statistics

---
+++ Front/src/Views/Statistics/StatisticsViews.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { PlatformStatisticsComponent } from "./PlatformStatistics";
+import { GenreStatisticsComponent } from "./genreStatistics";
+
+export const StatisticsViews = () => {
+  const [selectedComponent, setSelectedComponent] = useState(null);
+
+  const handleChange = (event) => {
+    setSelectedComponent(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <select value={selectedComponent} onChange={handleChange}>
+        <option value="">Select statistics</option>
+        <option value="platform">Games per platform</option>
+        <option value="genre">Games per genre</option>
+      </select>
+      {selectedComponent === "platform" && <PlatformStatisticsComponent />}
+      {selectedComponent === "genre" && <GenreStatisticsComponent />}
+    </div>
+  );
+};
