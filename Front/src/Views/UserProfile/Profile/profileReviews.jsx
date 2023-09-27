427e669bce93384ec59de76c320a3c4124e594a1
V C <107280002+VictorC1707@users.noreply.github.com>
2023-09-27 18:43:07 -0400

    Update profileReviews.jsx
    profileReview s

--- Front/src/Views/UserProfile/Profile/profileReviews.jsx
+++ Front/src/Views/UserProfile/Profile/profileReviews.jsx
@@ -17,15 +17,16 @@ const Review = () => {
   const[idReview, setIdReview] = useState("");
   const [isFormValid, setIsFormValid] = useState(false);
 
-  console.log(user.id, "userId" );
+  console.log(profile, "profile" );
+
 
 useEffect(()=>{
 dispatch(getUserByIdAction(user.id))
-}, [dispatch, profile.Review])
-
-  const handleEditClick = (id) => {
-    setSelectedReviewIndex(id);
-    const selectedReview = reviews[id];
+}, [dispatch])
+  
+  const handleEditClick = (index) => {
+    setSelectedReviewIndex(index);
+    const selectedReview = reviews[index];
     setIdReview(selectedReview.id)
     setEditedReview({
       rating: editedReview.rating,
@@ -37,17 +38,18 @@ dispatch(getUserByIdAction(user.id))
     try{
         const updatedReview = { ...reviews[selectedReviewIndex], ...editedReview };
         const update = await axios.put(`http://localhost:3001/reviews/${idReview}`, updatedReview)
+        dispatch(getUserByIdAction(user.id))
         setSelectedReviewIndex(-1);
+        setEditedReview({ rating: "", textReview: ""});
+        
+        
        }catch(error){
        console.log(JSON.stringify({error:error.message}))
        }
   };
-
   const handleCancelClick = () => {
     setSelectedReviewIndex(-1);
   };
-
-
 
 
   const handleInputChange = (e) => {
@@ -171,138 +173,5 @@ const handleRatingChange = (star) => {
   );
 };
   export default Review;
-
-
-
-
-// import { useState, useEffect } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faStar } from '@fortawesome/free-solid-svg-icons';
-
-
-// const Review = () => {
-
-//     const profile = useSelector((state)=>state.userProfile.userById)
-
-//     const review = profile.Reviews
-
-//   const [selectedReviewIndex, setSelectedReviewIndex] = useState(-1);
-//   const [editedReview, setEditedReview] = useState({ rating: "", textReview: "" });
-
-//   const handleEditClick = (index) => {
-//     setSelectedReviewIndex(index);
-//     const selectedReview = review[index];
-//     console.log(selectedReview, "select");
-//     setEditedReview({
-//       rating: selectedReview.rating,
-//       textReview: selectedReview.textReview,
-//     });
-//   };
-
-//   const handleSaveClick = () => {
-//     // Update the selected review with edited data
-//     const updatedReview = { ...reviews[selectedReviewIndex], ...editedReview };
-//     // Call an appropriate update function to save the edited review (e.g., dispatch an action in Redux)
-//     // For example, dispatch(updateReview(updatedReview));
-//     // After saving, clear the selected review
-//     setSelectedReviewIndex(-1);
-//   };
-
-//   const handleCancelClick = () => {
-//     // Clear the selected review without saving changes
-//     setSelectedReviewIndex(-1);
-//   };
-
-//   const handleInputChange = (e) => {
-//     const { name, value } = e.target;
-
-//     setEditedReview({
-//       ...editedReview,
-//       [name]: value,
-//     });
-//   };
-
-//   return (
-//     <div>
-//       {review.map((review, index) => (
-//         <div key={index}>
-//           <div className="review">
-//           <img src={review.Game.image} width={150} height={150} alt={review.Game.name} />
-//               {review.Game.name}
-//               <br />
-            
-//             {/* Review content */}
-//             {selectedReviewIndex === index ? (
-//               <div>
-//                 <form>
-//         <div>
-//           <label htmlFor="rating"></label>
-//           {[1, 2, 3, 4, 5].map((star) => (
-//             <span
-//               key={star}
-//               className={review.rating >= star ? 'marked' : 'star'}
-//               onClick={() => handleRatingChange(star)}
-//             >
-//               <FontAwesomeIcon icon={faStar} />
-//             </span>
-//           ))}
-//         </div>
-//         <div>
-//           <label htmlFor="reviewsText"></label>
-//           <textarea
-//             id="reviewsText"
-//             value={review.reviewsText}
-//             onChange={handleInputChange}
-//             rows={4}
-//           />
-//         </div>
-//         <div>
-//           {/* <button type="submit">Submit</button> */}
-//         </div>
-//       </form>
-//                 {/* Edit form with star rating and textReview */}
-//                 {/* ... */}
-//               </div>
-//             ) : (
-//               <div>
-//                 <div>
-//                   {review.textReview}
-//                   <br />
-//                   <div className="rating">
-//                     {Array.from({ length: 5 }, (_, starIndex) => (
-//                       <i
-//                         key={starIndex}
-//                         className={`fas fa-star star ${
-//                           starIndex < review.rating ? 'marked' : ''
-//                         }`}
-//                       ></i>
-//                     ))}
-//                   </div>
-//                 </div>
-//                 {/* Display review details */}
-                
-//                 {/* ... */}
-//               </div>
-//             )}
-//           </div>
-//           <br />
-//           {selectedReviewIndex === index ? (
-//             <div>
-//               <button onClick={handleSaveClick}>Save</button>
-//               <button onClick={handleCancelClick}>Cancel</button>
-//             </div>
-//           ) : (
-//             <button onClick={() => handleEditClick(index)}>Edit</button>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-  
-    
-//   };
-  
-//   export default Review;
 
 
