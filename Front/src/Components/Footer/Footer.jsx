3447b029351bf724a7292600c6738dfa61c231e5
V C <107280002+VictorC1707@users.noreply.github.com>
2023-09-28 02:33:29 -0400

    footer

--- Front/src/Components/Footer/Footer.jsx
+++ Front/src/Components/Footer/Footer.jsx
@@ -27,14 +27,13 @@ function Footer() {
   return (
     <div className="container-fluid nav-bar mt-auto">
   <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 mt-4 border-top">
-    <p className="col-md-4 mb-0 text-white">© 2023 8Bits, Inc</p>
+    <p className="col-md-4 mb-2 text-white">© 2023 8Bits, Inc</p>
 
-    <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+    <a href="/" className=" mb-2 col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
     <img  src={logo} width="80px" alt="" />
     </a>
 
-    <ul className="nav col-md-4 justify-content-end">
-      <li className="nav-item"><NavLink className='nav-link px-2 text-body-primary' onClick={() => window.scrollTo(0, 0)} to={ROUTES.HOME}>Home</NavLink></li>
+    <ul className="nav col-md-4 justify-content-end mb-2">
       <li className="nav-item"><NavLink className='nav-link px-2 text-body-primary' onClick={() => window.scrollTo(0, 0)} to={ROUTES.STORE}>Store</NavLink></li>
       <li className="nav-item"><NavLink className='nav-link px-2 text-body-primary' onClick={() => window.scrollTo(0, 0)} to={ROUTES.ABOUT}>About us</NavLink></li>
       <li className="nav-item"><NavLink className='nav-link px-2 text-body-primary' onClick={() => window.scrollTo(0, 0)} to={ROUTES.CONTACT}>Contact us</NavLink></li>
