bb2128dac63a7cb9133c09d3eb1ab82f8072742b
negrura14 <talleresmatador114@gmail.com>
2023-09-28 01:03:38 -0300

    success funcionando

--- Front/src/Components/MercadoPago/success/SuccessMP.jsx
+++ Front/src/Components/MercadoPago/success/SuccessMP.jsx
@@ -1,70 +1,66 @@
-import React, {useEffect} from "react";
+
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getGame } from '../../../Redux/gameActions';
-import axios from "axios";
+import axios from 'axios';
 import { UpdateList, cartUpdate } from "../../../Redux/Reducers/cartSlice";
+import queryString from 'query-string';
+
 
 
 const SuccessMP = () => {
   const location = useLocation();
   const dispatch = useDispatch();
-  const queryParams = new URLSearchParams(location.search);
   const { game } = useSelector(state => state.game);
-
+  const queryParams = queryString.parse(location.search);
+  const paymentId = queryParams.payment_id;
   const { user, auth } = useSelector((state) => state.user.userState);
   const userData = user;
-  const cart = JSON.parse(localStorage.getItem(`cart.${userData.user.id}`)) || [];
-
+  const [subtotal, setSubtotal] = useState(0);
 
   useEffect(() => {
     dispatch(getGame());
   }, [dispatch]);
 
+  let cart = JSON.parse(localStorage.getItem(`cart.${userData.user.id}`)) || [];
 
-  if (auth === true){
-    for (const element of cart) {
-      // console.log("ESTE ES EL RESULTADO DE UPDATELIST EN SUCCESSMP: ", element);
-      const gameStock = game.find((s) => s.id === element.id);
-      
-      if (gameStock && gameStock.stock > 0) {
-        const updatedGameStock = { ...gameStock, stock: gameStock.stock - 1 };        
-        console.log("Este es el nuevo stock: ", updatedGameStock);
-        axios.put(`/games/${updatedGameStock.id}`, updatedGameStock)
-      }
+  
+  const groupedCart = cart.reduce((acc, element) => {
+    if (acc[element.id]) {
+      acc[element.id].quantity += 1; 
+    } else {
+      acc[element.id] = { ...element, quantity: 1 };
     }
-  }
-  const deletedCArt = () => {
+    return acc;
+  }, {});
+
+ 
+  const calculateTotalPrice = (gameId, quantity) => {
+    const gameItem = game.find((s) => s.id === gameId);
+    return gameItem ? gameItem.price * quantity : 0;
+  };
+
+  
+  const calculateCartTotalPrice = () => {
+    let total = 0;
+  
+    Object.values(groupedCart).forEach((element) => {
+      total += calculateTotalPrice(element.id, element.quantity);
+    });
+  
+    return total;
+  };
+
+  useEffect(() => {
+    setSubtotal(calculateCartTotalPrice());
+  }, [groupedCart]);
+
+  const deletedCart = () => {
     localStorage.setItem(`cart.${userData.user.id}`, JSON.stringify([]));
     dispatch(UpdateList(userData.user.id));
     dispatch(cartUpdate());
-  }
-
-
-
-
-  const games = {};
-
-  for (let i = 0; i < queryParams.get("shoppingQuantity"); i++) {
-    const gameId = queryParams.get(`idGame${i}`);
-    const gameName = queryParams.get(`gameName${i}`);
-    const unitPrice = queryParams.get(`unitPrice${i}`);
-
-    if (!games[gameId]) {
-      
-      games[gameId] = {
-        gameId,
-        gameName,
-        unitPrice,
-        quantity: 1, 
-      };
-    } else {
-      
-      games[gameId].quantity += 1;
-    }
-  }
-
-  const gamesArray = Object.values(games); 
+  };
 
   return (
     <div className="container mt-5">
@@ -75,15 +71,18 @@ const SuccessMP = () => {
               <h2 className="card-title">Successful Payment</h2>
               <p className="card-text">Thanks for your purchase!</p>
               <h3 className="card-subtitle mb-3">Games Purchased:</h3>
-              {gamesArray.map((game, index) => (
+              {Object.values(groupedCart).map((element, index) => (
                 <div key={index}>
-                  <p className="card-text">Game ID: {game.gameId}</p>
-                  <p className="card-text">Game Name: {game.gameName}</p>
-                  <p className="card-text">Unit Price: ${game.unitPrice}</p>
-                  <p className="card-text">Quantity: {game.quantity}</p>
+                  <p className="card-text">Payment ID: {paymentId}</p>
+                  <p className="card-text">Game ID: {element.id}</p>
+                  <p className="card-text">Game Name: {element.name}</p>
+                  <p className="card-text">Unit Price: ${element.price}</p>
+                  <p className="card-text">Quantity: {element.quantity}</p>
+                  <p className="card-text">Total Price: ${calculateTotalPrice(element.id, element.quantity)}</p>
                 </div>
               ))}
-              <a href="/" className="btn btn-primary" onClick={deletedCArt}>
+              <p className="card-text">Cart Total Price: ${subtotal}</p>
+              <a href="/" className="btn btn-primary" onClick={deletedCart}>
                 Go to Home
               </a>
             </div>
@@ -95,5 +94,3 @@ const SuccessMP = () => {
 };
 
 export default SuccessMP;
-
-
