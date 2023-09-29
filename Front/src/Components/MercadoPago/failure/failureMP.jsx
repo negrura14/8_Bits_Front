339350fe51629884584af40103c262635a283aa3
negrura14 <talleresmatador114@gmail.com>
2023-09-28 22:52:13 -0300

    paga rechazado

--- Front/src/Components/MercadoPago/failure/failureMP.jsx
+++ Front/src/Components/MercadoPago/failure/failureMP.jsx
@@ -1,12 +1,38 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { ROUTES } from '../../../Helpers/RoutesPath'
 
 const FailureMP = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
+  const collectionStatus = queryParams.get("collection_status");
+  const paymentId = queryParams.get("payment_id");
+  const status = queryParams.get("status");
 
-  const failureReason = queryParams.get("failure_reason");
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false); // Estado de carga
 
+  useEffect(() => {
+    // Puedes agregar lógica adicional aquí, por ejemplo, guardar el registro de rechazo en tu base de datos si es necesario.
+  }, []);
+
+  const handleGoHomeClick = () => {
+    setIsLoading(true); // Establece isLoading en true al hacer clic
+    navigate("/");
+  };
+
+  const handleTryAgainClick = () => {
+    setIsLoading(true); // Establece isLoading en true al hacer clic
+    navigate(ROUTES.CHECKOUT);
+  };
+
+  // Muestra un mensaje de carga si isLoading es true
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Renderiza el componente normal si isLoading es false
   return (
     <div className="container mt-5">
       <div className="row">
@@ -14,11 +40,17 @@ const FailureMP = () => {
           <div className="card">
             <div className="card-body">
               <h2 className="card-title">Payment Rejected</h2>
-              <p className="card-text">Your payment was rejected for the following reason:</p>
-              <p className="card-text">{failureReason}</p>
-              <a href="/" className="btn btn-primary">
+              <p className="card-text">Your payment was rejected</p>
+              <p className="card-text">Payment ID: {paymentId}</p>
+              <p className="card-text">Status: {status}</p>
+
+              <button onClick={handleGoHomeClick} className="btn btn-primary">
                 Go to Home
-              </a>
+              </button>
+              <p>or</p>
+              <button onClick={handleTryAgainClick} className="btn btn-primary">
+                Try again
+              </button>
             </div>
           </div>
         </div>
@@ -28,3 +60,51 @@ const FailureMP = () => {
 };
 
 export default FailureMP;
+
+// import React, { useEffect } from "react";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import {ROUTES} from '../../../Helpers/RoutesPath'
+
+// const FailureMP = () => {
+//   const location = useLocation();
+//   const queryParams = new URLSearchParams(location.search);
+//   const collectionStatus = queryParams.get("collection_status");
+//   const paymentId = queryParams.get("payment_id");
+//   const status = queryParams.get("status");
+
+
+//   const navigate = useNavigate();
+  
+//   console.log('QUE MUESTRO ?',queryParams);
+
+//   useEffect(() => {
+//     // Puedes agregar lógica adicional aquí, por ejemplo, guardar el registro de rechazo en tu base de datos si es necesario.
+//   }, []);
+
+//   return (
+//     <div className="container mt-5">
+//       <div className="row">
+//         <div className="col-md-6 offset-md-3">
+//           <div className="card">
+//             <div className="card-body">
+//               <h2 className="card-title">Payment Rejected</h2>
+//               <p className="card-text">Your payment was rejected </p>
+//               <p className="card-text">Payment ID: {paymentId}</p>
+//               <p className="card-text">Status: {status}</p>
+              
+//               <button onClick={() => navigate("/")} className="btn btn-primary">
+//                 Go to Home
+//               </button>
+//               <p>or </p>
+//               <button onClick={() => navigate(ROUTES.CHECKOUT)} className="btn btn-primary">
+//                Try again
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FailureMP;
