1b8b490bebe3c65f05eb4d1826a785b9905e98e6
V C <107280002+VictorC1707@users.noreply.github.com>
2023-09-29 05:25:43 -0400

    estilos Failure

--- Front/src/Components/MercadoPago/failure/failureMP.jsx
+++ Front/src/Components/MercadoPago/failure/failureMP.jsx
@@ -34,28 +34,72 @@ const FailureMP = () => {
 
   // Renderiza el componente normal si isLoading es false
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <div className="card">
-            <div className="card-body">
-              <h2 className="card-title">Payment Rejected</h2>
-              <p className="card-text">Your payment was rejected</p>
-              <p className="card-text">Payment ID: {paymentId}</p>
-              <p className="card-text">Status: {status}</p>
+  <div class="container mt-5">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-10 backgroundinvoice">
+        
+        <div class="row">
+          <div class="col-sm-6">
+              <img class="imgFS" src="./images/Logo.png"/>
+          </div>
+         
+    
+          <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
+              <hr class="d-sm-none" />
+              <div class="text-white">
+                  <div class="mt-1 mb-2 text-600 ">
+                      Invoice
+                  </div>
+    
+                  <div class="my-2 "><i class="fa fa-circle text-secondary me-1"></i> <span class="text-600">Payment ID:</span> {paymentId}</div>
+    
+                  <div class="my-2"><i class="fa fa-circle text-secondary me-1"></i> <span class="text-600">Status:</span> <span class="text-danger">Rejected</span></div>
+                  <div class="my-2"><i class="fa fa-circle text-secondary me-1"></i> <span class="text-600">Reason:</span> <span>{status}</span></div>
+              </div>
+          </div>
+          
+      </div>
+    
+      <div class="mt-4 ">
+        
+          <div class="row border-b-2 brc-default-l2 d-flex"></div>
+          <div class="d-flex justify-content-center">
 
-              <button onClick={handleGoHomeClick} className="btn btn-primary">
-                Go to Home
-              </button>
-              <p>or</p>
-              <button onClick={handleTryAgainClick} className="btn btn-primary">
-                Try again
-              </button>
+            <div class=" divPaymentError d-flex justify-content-center align-items-center text-white">
+              <div>
+                <h2 className="card-title text-white">Payment Rejected</h2>
+                <p className="card-text text-white">Your payment was rejected</p>
+              </div>
+                
             </div>
           </div>
-        </div>
+    
+    
+          
+    
+          <hr />
+    
+          <div class="d-flex justify-content-center">
+            <button onClick={handleGoHomeClick} class="btn btn-outline-primary text-white bg-transparent px-4 ">
+              Go to Home
+            </button>
+            <div class="d-flex justify-content-center align-items-center mx-4 p-2">
+              <p class="m-0">or</p>
+
+            </div>
+            <button onClick={handleTryAgainClick} class="btn btn-outline-primary text-white bg-transparent px-4 ">
+              Try again
+            </button>
+             
+              
+          </div>
+        
+
       </div>
     </div>
+  </div>
+</div>
+
   );
 };
 
